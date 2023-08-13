ea523590280c58a7d55ff5421a7acae1ec92c5b2
fuxiaoming <fuxiaoming@kingsoft.com>
2023-08-13 12:56:45 +0800

    add : 添加脚本打包插件

--- bin/start.sh
+++ bin/start.sh
@@ -2,7 +2,8 @@
 # Start the application
 # This script is called by the Dockerfile
 #一键启动脚本
-#启动metaserver
-java -jar ../metaserver/target/metaServer-1.0.jar &
-#启动从metaserver
-java -jar ../metaserver/target/metaServer-1.0.jar &
+#启动metaserver 日志输出到log 添加zookeeper -Dzookeeper.addr=10.243.137.17
+java -Dzookeeper.addr=10.243.137.17 -jar ../metaServer/metaServer-1.0.jar > "../metaServer/log" 2>&1 &
+#启动从metaserver指定8001端口--server.port=8001  -Dzookeeper.addr=10.243.137.17
+
+java -Dzookeeper.addr=10.243.137.17 -jar ../metaServer/metaServer-1.0.jar --server.port=8001 > "../metaServer/log2" 2>&1 &
