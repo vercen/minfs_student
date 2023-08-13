6a765e082644cc2f60099055705ade9fa6f3353f
fuxiaoming <fuxiaoming@kingsoft.com>
2023-08-13 13:23:19 +0800

    fix : 修复测试bug

--- bin/start.sh
+++ bin/start.sh
@@ -6,4 +6,9 @@
 java -Dzookeeper.addr=10.243.137.17 -jar ../metaServer/metaServer-1.0.jar > "../metaServer/log" 2>&1 &
 #启动从metaserver指定8001端口--server.port=8001  -Dzookeeper.addr=10.243.137.17
 
-java -Dzookeeper.addr=10.243.137.17 -jar ../metaServer/metaServer-1.0.jar --server.port=8001 > "../metaServer/log2" 2>&1 &
+java -Dzookeeper.addr=10.243.137.17 -jar ../metaServer/metaServer-1.0.jar --server.port=8001 > "../metaServer/log1" 2>&1 &
+#启动dataserver 9000 -9003
+java -Dzookeeper.addr=10.243.137.17 -jar ../dataServer/dataServer-1.0.jar --server.port=9000 > "../dataServer/log" 2>&1 &
+java -Dzookeeper.addr=10.243.137.17 -jar ../dataServer/dataServer-1.0.jar --server.port=9001 > "../dataServer/log1" 2>&1 &
+java -Dzookeeper.addr=10.243.137.17 -jar ../dataServer/dataServer-1.0.jar --server.port=9002 > "../dataServer/log2" 2>&1 &
+java -Dzookeeper.addr=10.243.137.17 -jar ../dataServer/dataServer-1.0.jar --server.port=9003 > "../dataServer/log3" 2>&1 &
